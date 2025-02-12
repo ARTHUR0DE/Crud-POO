4a271ac94c43e8646738bff0e9b1dbb18397ebee
ARTHUR0DE <antunesarthur43@gmail.com>
2025-02-12 00:03:18 +0000

    aula 3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { AlunoController } from "./modulos/Aluno/controllers"
+import PromptSync from "prompt-sync"
+import { AlunoController } from "./modulos/Aluno/controllers/index"
 
 const aluno1 = new AlunoController()
 
@@ -12,3 +13,37 @@ aluno1.listarTodos()
 
 aluno1.editar('a9222',null,"borgeseditado@edum.senac.br",null)
 aluno1.listarTodos()
+
+export default function menuPrincipal() {
+    const imput = prompt()
+    const opcoes = [
+        "1 - criar aluno",
+        "2 - Listar todos os alunos",
+        "3 - Editar aluno por matrícula",
+        "4 - Excluir todos os alunos",
+        "5 - Excluir por matrícula",
+        "0 - Sair"
+    ]
+    console.log(opcoes)
+    let opcao = imput("Escolha a opção desejada: ")
+    console.clear()
+    switch(opcao) {
+        case "0":
+            break
+        case "1":
+            criar()
+            break
+        case "2":
+            listaPorMatricula()
+            break
+        case "3":
+            editar()
+            break
+        case "4":
+            deletarTodos()
+          break
+        case "5":
+          deletarPorMatricula()
+          break  
+    }
+}
